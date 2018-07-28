3ae839616cc026926efec8c3ce4fd334d2907e01
ratmirslv <vf7x@mail.ru>
2018-07-29 00:28:34 +0300

    task1

--- src/components/TicketsFilter.js
+++ src/components/TicketsFilter.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Filterstyle from "./TicketsFilter.css";
+import filterStyle from "./TicketsFilter.css";
 
 class TicketsFilter extends Component {
   constructor(props) {
@@ -8,9 +8,71 @@ class TicketsFilter extends Component {
   }
   render() {
     return (
-      <div className="Filterstyle">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad accusantium
-        libero provident tempora sunt amet ratione quasi adipisci magni nemo?
+      <div className="filterStyle">
+        <div className="currencies">
+          <p className="filterHeader">Валюта</p>
+          <button className="currenciesName">rub</button>
+          <button className="currenciesName">usd</button>
+          <button className="currenciesName">eur</button>
+        </div>
+        <div className="filterTransfers">
+          <p className="filterHeader">Количество пересадок</p>
+          <form className="filterItem">
+            <label>
+              <input
+                name="all"
+                type="checkbox"
+                checked={this.state.isGoing}
+                onChange={this.handleInputChange}
+              />
+            </label>
+            Все
+          </form>
+          <form className="filterItem">
+            <label>
+              <input
+                name="one"
+                type="checkbox"
+                checked={this.state.isGoing}
+                onChange={this.handleInputChange}
+              />
+              1 Пересадка
+            </label>
+          </form>
+          <form className="filterItem">
+            <label>
+              <input
+                name="two"
+                type="checkbox"
+                checked={this.state.isGoing}
+                onChange={this.handleInputChange}
+              />
+              2 Пересадки
+            </label>
+          </form>
+          <form className="filterItem">
+            <label>
+              <input
+                name="three"
+                type="checkbox"
+                checked={this.state.isGoing}
+                onChange={this.handleInputChange}
+              />
+              3 Пересадки
+            </label>
+          </form>
+          <form className="filterItem">
+            <label>
+              <input
+                name="four"
+                type="checkbox"
+                checked={this.state.isGoing}
+                onChange={this.handleInputChange}
+              />
+              4 Пересадки
+            </label>
+          </form>
+        </div>
       </div>
     );
   }
