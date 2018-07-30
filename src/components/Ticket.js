5ffe623aa37641c8a7660e47c1b0c8995dd81216
ratmirslv <vf7x@mail.ru>
2018-07-31 00:45:33 +0300

    style tickets

--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -12,28 +12,30 @@ class Ticket extends Component {
     return (
       <div className="ticketStyle">
         <div className="ticketBuy">
-          <button className="button">{`Купить за ${ticket.price}`}</button>
+          <button className="button">
+            Купить <br /> за {"\n" + ticket.price + " \u20BD"}
+          </button>
         </div>
         <div className="ticketInfo">
           <div className="departure">
-            <p className="departureTime">{ticket.departure_time}</p>
-            <p className="departureCity">
+            <div className="departureTime">{ticket.departure_time}</div>
+            <div className="departureCity">
               {`${ticket.origin}, ${ticket.origin_name}`}
-            </p>
-            <p className="departureDate">{ticket.departure_date}</p>
+            </div>
+            <div className="departureDate">{ticket.departure_date}</div>
           </div>
           <div className="transfers">
-            <p className="transfersInfo">{`${ticket.stops} Пересадок`}</p>
+            <div className="transfersInfo">{`${ticket.stops} Пересадок`}</div>
             <div className="transfersLine">
               <img src={lineairplane} alt="airplane" />
             </div>
           </div>
           <div className="destination">
-            <p className="destinationTime">{ticket.arrival_time}</p>
-            <p className="destinationCity">
+            <div className="destinationTime">{ticket.arrival_time}</div>
+            <div className="destinationCity">
               {`${ticket.destination}, ${ticket.destination_name}`}
-            </p>
-            <p className="destinationDate">{ticket.arrival_date}</p>
+            </div>
+            <div className="destinationDate">{ticket.arrival_date}</div>
           </div>
         </div>
       </div>
